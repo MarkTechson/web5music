0282a1974145fad93ed31056cdce438be0fe23c8
Mark Thompson <2554588+MarkTechson@users.noreply.github.com>
2023-07-20 06:05:09 -0700

    (app): refactor the app
    * Adds support for refreshing the token on each call
    * Adds a write up on the README and links
    * Updates the UI code to support all of the requirements
    * Adding a little love and magic

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.use(express.static(path.join(__dirname, 'web')));
 
 let spotifyToken;
 
+/**
+ * Requests the access token from spotify
+ * 
+ * @returns a promise
+ */
 function fetchAccessToken() {
     return fetch(ACCESS_TOKEN_URL, {
         method: 'POST',
@@ -21,10 +26,18 @@ function fetchAccessToken() {
         return response.json();
     }).then(tokenResponse => {
         spotifyToken = tokenResponse.access_token;
-        console.log('➡️ spotify token acquired');
+        console.log('➡️ Spotify token acquired');
     });
 }
 
+/**
+ * searches the spotify API for an artist and track combination. Tries
+ * to match a track. But if one is not found, we still return a valid response
+ * 
+ * @param {string} artist 
+ * @param {string} track 
+ * @returns an object representing the matching track
+ */
 async function searchSpotify(artist, track) {
     const baseURL = `https://api.spotify.com/v1/search?`;
     const santizedParams = new URLSearchParams(`q=artist:${artist} track:${track}&type=track`);
@@ -33,24 +46,32 @@ async function searchSpotify(artist, track) {
     /**
      * Request the data from Spotify
      */
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${spotifyToken}`
-        }
-    });
+    let response;
+    try {
+        response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${spotifyToken}`
+            }
+        });
+    } catch (error) {
+        console.log('an error occurred', error);
+    }
 
     const data = await response.json();
-    const tracks = data.tracks.items.filter(aTrack => aTrack.name.toLowerCase() === track.toLowerCase());
+    console.log(data);
+    let tracks = [];
+
+    if (data.tracks) {
+        tracks = (data.tracks.items || []).filter(aTrack => aTrack.name.toLowerCase() === track.toLowerCase());
+    }
 
     if (tracks.length > 0) {
         console.log('✅ found a matching track');
     } else {
         return {
             album: {
-                images: [
-                    'path/to/default/photo'
-                ]
+                images: ['']
             }
         };
     }
@@ -58,20 +79,25 @@ async function searchSpotify(artist, track) {
     return tracks[0];
 }
 
-
+/**
+ * API Endpoint: /music-info
+ * 
+ * Refreshes the API token and then makes a call to the spotify API
+ */
 app.get('/music-info', async (req, res) => {
-    if (!spotifyToken) {
-        /**
-         * If the token has not be set (i.e., requested), make a call to request
-         * the token.
-         *  
-         * TODO: nice to have, set a timer for 3600 seconds because the life
-         * of the token is only 3600 seconds and would need to be refeshed
-         */
-        await fetchAccessToken();
-    }
 
-    const {artist, track} = req.query;
+    /**
+     * the token being used doesn't auto refresh and expires after an hour, so we will
+     * request it every time but in a real world scenario we'd handle this gracefully
+     * 
+     * For example:
+     * using a auth flow that refreshed https://developer.spotify.com/documentation/web-api/tutorials/code-flow
+     * 
+     * (but since this example doesn't have to request user access, we'll do this)
+     */
+
+    await fetchAccessToken();
+    const { artist, track } = req.query;
     const trackData = await searchSpotify(artist, track);
 
     res.json(trackData);
--- web/app.js
+++ web/app.js
@@ -3,11 +3,16 @@ import { Web5 } from 'https://cdn.jsdelivr.net/npm/@tbd54566975/web5@0.7.11/dist
 const MUSIC_GROUP_SCHEMA_URL = 'https://schema.org/MusicGroup';
 let web5;
 let did;
-let spotifyToken;
+let rowsToCreate;
 
 document.getElementById('saveBtn').onclick = createEntry;
 window.onload = init;
 
+/**
+ * Initialization function that connects to Web5, queries the DWN data,
+ * and displays any saved data.
+ * @returns void
+ */
 async function init() {
   /**
    * If the Web5 import failed for some reason, pass a warning to the
@@ -41,75 +46,121 @@ async function init() {
     }
   });
 
-  const recordJson = await records[0].data.json();
-  console.log(recordJson);
-  displaySavedSong(recordJson);
+  records.forEach(async (record) => {
+    const recordJson = await record.data.json();
+    displaySavedSong(recordJson);
+  });
+
+  rowsToCreate = 3 - records.length;
+  createTrackEntryFormRows(rowsToCreate);
 
   /**
    * Enable the save button once the intial dataload has been completed
    */
-  document.getElementById('saveBtn').disabled = false;
+  document.getElementById('saveBtn').disabled = rowsToCreate === 0 ? true : false;
+  document.getElementById('main').style.display = 'initial';
+  document.getElementById('loading-message').style.display = 'none';
 }
+
+/**
+ * Create a DOM entry for a track based on the
+ * provided json parameter.
+ * @param {*} json track data matching the schema
+ */
 function displaySavedSong(json) {
-  // ideally, you would validate that the JSON here is matching the schema
-  const section = document.createElement('section');
-  const artistTag = document.createElement('p');
-  const songTag = document.createElement('p');
-  const albumArtTag = document.createElement('img');
+  const trackList = document.getElementById('track-list');
+  const range = document.createRange();
+  range.selectNode(trackList);
 
-  artistTag.textContent = json.name;
-  songTag.textContent = json.track[0].name;
-  
-  albumArtTag.setAttribute('src', json.image || '/assets/default_image.jpg');
-  albumArtTag.setAttribute('width', 200);
-  section.appendChild(artistTag);
-  section.appendChild(songTag);
-  section.appendChild(albumArtTag);
-
-  document.getElementById('song-list').appendChild(section);
+  const documentFragment = range.createContextualFragment(`
+    <section class="track-card">
+      <img src="${json.image || '/assets/default_image.jpg'}" width="200">
+      <div>
+        <p class="track-artist">${json.name}</p>
+        <p class="track-name">${json.track[0].name}</p>
+      </div>
+    </section>
+  `);
+  trackList.appendChild(documentFragment);
 }
 
+/**
+ * Create a new DWN entry for each of the populated
+ * artist and track input fields. Note, if one of the input
+ * fields is missing, that entry will be ignored
+ * @param {ClickEvent} e the click event 
+ */
 async function createEntry(e) {
   e.preventDefault();
 
-  const { value: artist } = document.getElementById('artist-name-1');
-  const { value: track } = document.getElementById('artist-song-1');
+  /**
+   * For each of the elements on the screen get the artist name and song and
+   * make the required queries. 
+   */
+  const trackEntryForm = document.getElementById('track-entry-form');
 
-/**
- * Santize the inputs and ensure that they are not blank before adding them to the data.
- */
+  for (let i = 0; i < rowsToCreate; i++) {
+    const artistNode = document.getElementById(`artist-name-${i}`);
+    const artistTrack = document.getElementById(`artist-track-${i}`);
+    const trackEntryRow = document.getElementById(`track-entry-row-${i}`);
 
-// if (artist && artist.trim())
-// if (track && track.trim())
+    if (artistNode === null || artistTrack === null) continue;
 
-  // search spotify for the artist and song combination
-  const santizedParams = new URLSearchParams(`artist=${artist}&track=${track}`);
-  const response = await fetch(`/music-info?${santizedParams.toString()}`);
-  const trackData = await response.json();
-  const image = trackData.album.images[0].url;
+    let { value: artist } = artistNode;
+    let { value: track } = artistTrack;
 
-  document.getElementById('cover-art-1').setAttribute('src', trackData.album.images[0].url);
+    artist = artist.trim();
+    track = track.trim();
 
-  const json = convertToSchema(artist, track, image);
+    /**
+     * if the data for the track is empty or incomplete, go to the next available
+     * set of nodes.
+     */
+    if (!artist || !track) continue;
+    console.log(`artist "${artist}", track "${track}"`);
 
-  // attempt to fetch the data from spotify
-  // Write to the DWN record
-  // const { record } = await web5.dwn.records.create({
-  //   data: json,
-  //   message: {
-  //     schema: MUSIC_GROUP_SCHEMA_URL,
-  //     dataFormat: 'application/json'
-  //   }
-  // });
+    const santizedParams = new URLSearchParams(`artist=${artist}&track=${track}`);
 
-  console.log(record);
+    // search spotify for the artist and song combination
+    const response = await fetch(`/music-info?${santizedParams.toString()}`);
+    const trackData = await response.json();
+    const image = trackData.album.images[0].url;
+    const json = convertToSchema(artist, track, image);
+
+    // Write to the DWN record
+    const { record } = await web5.dwn.records.create({
+      data: convertToSchema(artist, track, image),
+      message: {
+        schema: MUSIC_GROUP_SCHEMA_URL,
+        dataFormat: 'application/json'
+      }
+    });
+
+    // Adding a force send here because the wait for the sync
+    // may convince the user that something didn't work
+    try {
+      const { status } = await record.send(did);
+    } catch (e) {
+      console.log('an error occured during `send` while attempting to write to the DWN', e);
+    }
+
+    displaySavedSong(json);
+    trackEntryForm.removeChild(trackEntryRow);
+  }
 }
 
+/**
+ * 
+ * @param {String} artist name of the artist
+ * @param {String} track name of the track
+ * @param {String} image album cover image (if not provided will use default)
+ * @returns JSON string in the format of the schema
+ */
 function convertToSchema(artist, track, image) {
   return {
     '@type': 'MusicGroup',
     'name': artist,
-    'image': image ?? 'default_image.jpg',
+    'image': image ?? '/assets/default_image.jpg',
     'track': [
       {
         '@type': 'MusicRecording',
@@ -117,4 +168,60 @@ function convertToSchema(artist, track, image) {
       }
     ],
   };
+}
+
+/**
+ * Deletes the entries from the DWN
+ */
+async function deleteEntries() {
+  const { records } = await web5.dwn.records.query({
+    from: did,
+    message: {
+      filter: {
+        schema: MUSIC_GROUP_SCHEMA_URL
+      }
+    }
+  });
+
+  for (let i = 0; i < records.length; i++) {
+    try {
+      await records[i].delete();
+      const { status } = await records[i].send(did);
+      console.log('send status:', status);
+    } catch (e) {
+      console.log('error occurred during `send` after attempting to delete', e);
+    }
+  }
+
+  // Display track entry forms
+  createTrackEntryFormRows(3);
+  // remove the entries from the UI
+  document.getElementById('track-list').innerHTML = '';
+
+  console.log('All DWN entries have been deleted');
+}
+
+/**
+ * Utility function to create track entry rows for users
+ * to input the track name and the artist name
+ * 
+ * @param {Number} rowsToCreate number of rows to create
+ */
+function createTrackEntryFormRows(rowsToCreate) {
+  const formElement = document.getElementById('track-entry-form');
+
+  for (let i = 0; i < rowsToCreate; i++) {
+    const range = document.createRange();
+    range.selectNode(formElement);
+    const documentFragment = range.createContextualFragment(`
+      <div id="track-entry-row-${i}" class="track-entry">
+        <label for="artist-name-${i}">Artist Name</label>
+        <input type="text" id="artist-name-${i}" placeholder="Artist Name">
+
+        <label for="artist-track-${i}">Artist Song</label>
+        <input type="text" id="artist-track-${i}" placeholder="Track Name">
+      </div>
+    `);
+    formElement.prepend(documentFragment);
+  }
 }
