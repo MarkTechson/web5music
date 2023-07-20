05c44a7199a6bd6aa4b6e0f18d641cefebdea478
Mark Thompson <2554588+MarkTechson@users.noreply.github.com>
2023-07-20 00:26:43 -0700

    setup spotify data on the node side of the app and some more refactoring.

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-console.log(process.env.MYVAL);
+const ACCESS_TOKEN_URL = 'https://accounts.spotify.com/api/token';
 
 app.use(express.static(path.join(__dirname, 'web')));
 
-app.listen(port, ()=> {
+let spotifyToken;
+
+function fetchAccessToken() {
+    return fetch(ACCESS_TOKEN_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `grant_type=client_credentials&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
+    }).then(response => {
+        return response.json();
+    }).then(tokenResponse => {
+        spotifyToken = tokenResponse.access_token;
+        console.log('➡️ spotify token acquired');
+    });
+}
+
+async function searchSpotify(artist, track) {
+    const baseURL = `https://api.spotify.com/v1/search?`;
+    const santizedParams = new URLSearchParams(`q=artist:${artist} track:${track}&type=track`);
+    const url = baseURL + santizedParams.toString();
+
+    /**
+     * Request the data from Spotify
+     */
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${spotifyToken}`
+        }
+    });
+
+    const data = await response.json();
+    const tracks = data.tracks.items.filter(aTrack => aTrack.name.toLowerCase() === track.toLowerCase());
+
+    if (tracks.length > 0) {
+        console.log('✅ found a matching track');
+    } else {
+        return {
+            album: {
+                images: [
+                    'path/to/default/photo'
+                ]
+            }
+        };
+    }
+
+    return tracks[0];
+}
+
+
+app.get('/music-info', async (req, res) => {
+    if (!spotifyToken) {
+        /**
+         * If the token has not be set (i.e., requested), make a call to request
+         * the token.
+         *  
+         * TODO: nice to have, set a timer for 3600 seconds because the life
+         * of the token is only 3600 seconds and would need to be refeshed
+         */
+        await fetchAccessToken();
+    }
+
+    const {artist, track} = req.query;
+    const trackData = await searchSpotify(artist, track);
+
+    res.json(trackData);
+});
+
+app.listen(port, () => {
     console.log('🌍 Server listening');
 })
 
--- web/app.js
+++ web/app.js
@@ -4,100 +4,93 @@ const MUSIC_GROUP_SCHEMA_URL = 'https://schema.org/MusicGroup';
 let web5;
 let did;
 let spotifyToken;
-const ACCESS_TOKEN_URL = 'https://accounts.spotify.com/api/token';
 
 document.getElementById('saveBtn').onclick = createEntry;
 window.onload = init;
 
 async function init() {
-  // get spotify access key
-  const accessTokenResponse = await fetch(ACCESS_TOKEN_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: ''
-  });
-
-  spotifyToken = (await accessTokenResponse.json()).access_token;
-
-  return;
   /**
    * If the Web5 import failed for some reason, pass a warning to the
    * user and return.
    */
-    if (!Web5) {
-        console.log('⚠️ Unabled to load Web5');
-        return;
+  if (!Web5) {
+    console.log('⚠️ Unabled to load Web5');
+    return;
+  }
+  console.log('✅ Web5 loaded successfully');
+
+  /**
+   * Using the connect method on the Web5 import to
+   * create a web5 instance and get access to the existing DID
+   * or create a new one.
+   */
+  const response = await Web5.connect();
+  web5 = response.web5;
+  did = response.did;
+
+  /**
+   * Query the DWN (decentralized web nodes) for existing data. Filter the
+   * records returned by the schema type which is MusicRecording.
+   */
+  const { records } = await web5.dwn.records.query({
+    from: did,
+    message: {
+      filter: {
+        schema: MUSIC_GROUP_SCHEMA_URL
+      }
     }
-    console.log('✅ Web5 loaded successfully');
+  });
 
-    /**
-     * Using the connect method on the Web5 import to
-     * create a web5 instance and get access to the existing DID
-     * or create a new one.
-     */
-    const response = await Web5.connect();
-    web5 = response.web5;
-    did = response.did;
+  const recordJson = await records[0].data.json();
+  console.log(recordJson);
+  displaySavedSong(recordJson);
 
-    /**
-     * Query the DWN (decentralized web nodes) for existing data. Filter the
-     * records returned by the schema type which is MusicRecording.
-     */
-    const { records } = await web5.dwn.records.query({
-        from: did,
-        message: {
-            filter: {
-              schema: MUSIC_GROUP_SCHEMA_URL
-            }
-        }
-    });
-  
-    const recordJson = await records[0].data.json();
-    console.log(recordJson);
-    displaySavedSong(recordJson);
-
-    /**
-     * Enable the save button once the intial dataload has been completed
-     */
-    document.getElementById('saveBtn').disabled = false;
+  /**
+   * Enable the save button once the intial dataload has been completed
+   */
+  document.getElementById('saveBtn').disabled = false;
 }
 function displaySavedSong(json) {
   // ideally, you would validate that the JSON here is matching the schema
   const section = document.createElement('section');
   const artistTag = document.createElement('p');
   const songTag = document.createElement('p');
-  const albumArt = document.createElement('img');
+  const albumArtTag = document.createElement('img');
 
   artistTag.textContent = json.name;
   songTag.textContent = json.track[0].name;
-
+  
+  albumArtTag.setAttribute('src', json.image || '/assets/default_image.jpg');
+  albumArtTag.setAttribute('width', 200);
   section.appendChild(artistTag);
   section.appendChild(songTag);
-  // section.appendChild(albumArt);
+  section.appendChild(albumArtTag);
 
   document.getElementById('song-list').appendChild(section);
 }
 
 async function createEntry(e) {
   e.preventDefault();
-  
-  const {value: artistName} = document.getElementById('artist-name-1');
-  const {value: artistSong} = document.getElementById('artist-song-1');
+
+  const { value: artist } = document.getElementById('artist-name-1');
+  const { value: track } = document.getElementById('artist-song-1');
+
+/**
+ * Santize the inputs and ensure that they are not blank before adding them to the data.
+ */
+
+// if (artist && artist.trim())
+// if (track && track.trim())
 
   // search spotify for the artist and song combination
-  const trackData = await searchSpotify(artistName, artistSong);
-  console.log(trackData.album.images[0]);
-  return;
-  /**
-   * Santize the inputs and ensure that they are not blank before adding them to the data.
-   */
-  
-  // if (artistName && artistName.trim())
-  // if (artistSong && artistSong.trim())
+  const santizedParams = new URLSearchParams(`artist=${artist}&track=${track}`);
+  const response = await fetch(`/music-info?${santizedParams.toString()}`);
+  const trackData = await response.json();
+  const image = trackData.album.images[0].url;
 
-  const json = convertToSchema(artistName, artistSong);
+  document.getElementById('cover-art-1').setAttribute('src', trackData.album.images[0].url);
+
+  const json = convertToSchema(artist, track, image);
 
   // attempt to fetch the data from spotify
   // Write to the DWN record
@@ -112,49 +105,16 @@ async function createEntry(e) {
   console.log(record);
 }
 
-async function searchSpotify(artistName, artistSong) {
-  const baseURL =`https://api.spotify.com/v1/search?`;
-  const santizedParams = new URLSearchParams(`q=artist:${artistName} track:${artistSong}&type=track`);
-  const url = baseURL+santizedParams.toString();
-
-  /**
-   * Request the data from Spotify
-   */
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${spotifyToken}`
-    }
-  });
-
-  const data = await response.json();
-  const tracks = data.tracks.items.filter(track => track.name.toLowerCase() === artistSong.toLowerCase());
-
-  if (tracks.length > 0) {
-    console.log('✅ found a matching track');
-  } else {
-    return {
-      album: {
-        images: [
-          'path/to/default/photo'
-        ]
-      }
-    };
-  }
-
-  return tracks[0];
-}
-
-function convertToSchema (artistName, artistSong) {
+function convertToSchema(artist, track, image) {
   return {
     '@type': 'MusicGroup',
-    'name': artistName,
-    'image': image ?? 'default-image.png',
+    'name': artist,
+    'image': image ?? 'default_image.jpg',
     'track': [
       {
         '@type': 'MusicRecording',
-        'name': artistSong
+        'name': track,
       }
-    ]
+    ],
   };
 }
